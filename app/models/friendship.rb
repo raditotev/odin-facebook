3c38edef6fa669993a0fb893a1f9bf86299d502d
raditotev <raditotev@gmail.com>
2015-12-11 13:25:15 +0000

    Added Friendship model and table

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,14 @@
 class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, class_name: "User"
+
+  after_create :ensure_reverse_friendship
+
+  private
+
+  def ensure_reverse_friendship
+    unless Friendship.exists?(user: self.friend, friend: self.user)
+      Friendship.create(user: self.friend, friend: self.user)
+    end
+  end
 end
