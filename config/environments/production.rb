18c8806d2e733319db2503ccb8d274fb81ae63c3
raditotev <raditotev@gmail.com>
2016-01-20 13:51:47 +0000

    Added secret_key_base for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Secret key base for production
+  config.secret_key_base = ENV[“SECRET_KEY_BASE”]
 end
