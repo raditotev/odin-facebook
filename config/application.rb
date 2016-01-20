efda748838bece87f03c75984de69342b3617d88
raditotev <raditotev@gmail.com>
2016-01-20 13:08:14 +0000

    Stopped assets precompile

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,10 @@ module FacebookApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # Path to notifiable.rb hook
     config.autoload_paths<<Rails.root.join('lib')
+    # Stop assets precompile
+    config.assets.initialize_on_precompile = false
+
   end
 end
