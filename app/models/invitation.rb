b4fb6ea1ebd0506265efe565898d063fb86e3e76
raditotev <raditotev@gmail.com>
2016-01-24 16:04:31 +0000

    Add hook for creating Friendship

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,6 +1,16 @@
 class Invitation < ActiveRecord::Base
   include Notifiable
 
+  after_update :create_friendship
+
   belongs_to :from_user, class_name: "User"
   belongs_to :to_user, class_name: "User"
+
+  private
+
+  def create_friendship
+    if self.approved_changed? && self.approved == true
+      Friendship.create!(user: from_user, friend: to_user)
+    end
+  end
 end
